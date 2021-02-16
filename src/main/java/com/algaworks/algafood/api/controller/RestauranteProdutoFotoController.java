eb41ac89f7646e9061f002ce9ea0974725221480
BenjaminFernandes <benjamin_fernandes10@hotmail.com>
2021-02-16 17:29:37 -0300

    Implementando o storage local para armazenar as fotos dos produtos

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +33,7 @@ public class RestauranteProdutoFotoController {
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, 
-			@Valid FotoProdutoInput fotoProdutoInput ) {
+			@Valid FotoProdutoInput fotoProdutoInput ) throws IOException {
 		
 		Produto produto = this.cadastroProdutoService.buscarOuFalhar(produtoId);
 		MultipartFile arquivo = fotoProdutoInput.getArquivo();
@@ -43,7 +45,7 @@ public class RestauranteProdutoFotoController {
 		foto.setTamanho(arquivo.getSize());
 		foto.setNomeArquivo(arquivo.getOriginalFilename());
 		
-		FotoProduto fotoSalva = this.catalogoFotoProduto.salvar(foto);
+		FotoProduto fotoSalva = this.catalogoFotoProduto.salvar(foto, arquivo.getInputStream());
 		
 		return fotoProdutoModelAssembler.toModel(fotoSalva);
 	}
--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -1,21 +1,49 @@
 package com.algaworks.algafood.domain.service;
 
+import java.io.InputStream;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
+import com.algaworks.algafood.domain.service.FotoStorageService.NovaFoto;
 
 @Service
 public class CatalogoFotoProdutoService {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private FotoStorageService fotoStorageService;
 	
 	@Transactional
-	public FotoProduto salvar(FotoProduto foto) {
+	public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
+		Long restauranteId = foto.getRestauranteId();
+		Long produtoId = foto.getProduto().getId();
+		String novoNomeArquivo = fotoStorageService.gerarNomeArquivo(foto.getNomeArquivo()); 
+		String nomeArquivoExistente = null;
 		
-		return produtoRepository.save(foto);
+		Optional<FotoProduto> fotoExistente = this.produtoRepository
+				.findFotoById(restauranteId, produtoId);
+		
+		if(fotoExistente.isPresent()) {
+			nomeArquivoExistente = fotoExistente.get().getNomeArquivo();
+			this.produtoRepository.delete(fotoExistente.get());
+		}
+		
+		foto.setNomeArquivo(novoNomeArquivo);
+		foto = produtoRepository.save(foto);
+		produtoRepository.flush();
+		
+		NovaFoto novaFoto = NovaFoto.builder()
+				.nomeArquivo(foto.getNomeArquivo())
+				.inputStream(dadosArquivo)
+				.build();
+		
+		this.fotoStorageService.substituir(nomeArquivoExistente, novaFoto);
+		return foto;
 	}
 }
