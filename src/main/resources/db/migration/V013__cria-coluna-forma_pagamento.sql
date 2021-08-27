20c5c52dcd68242badb51c0196812937031ef0dd
BenjaminFernandes <benjamin_fernandes10@hotmail.com>
2021-08-26 23:14:28 -0300

    Adicionando biblioteca SpringFox. Especificação OpenApi

--- src/main/resources/db/migration/V013__cria-coluna-forma_pagamento.sql
+++ src/main/resources/db/migration/V013__cria-coluna-forma_pagamento.sql
@@ -1,3 +1,2 @@
 ALTER TABLE forma_pagamento ADD data_atualizacao datetime null;
 UPDATE forma_pagamento SET data_atualizacao = utc_timestamp;
-ALTER TABLE forma_pagamento MODIFY data_atualizacao not null;
