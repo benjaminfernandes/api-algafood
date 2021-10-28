7d5198f0f8e94741fcc60e33378d7ca7a44303d8
BenjaminFernandes <benjamin_fernandes10@hotmail.com>
2021-10-27 22:53:16 -0300

    Adicionando hypermedia na representação de recurso único com HAL

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -65,7 +66,18 @@ public class CidadeController implements CidadeControllerOpenApi {
 	@GetMapping("/{cidadeId}")
 	public CidadeModel buscar(@PathVariable Long cidadeId) {
 		Cidade cidade = this.cadastroCidadeService.buscarOuFalhar(cidadeId);
-		return this.cidadeConverter.toModel(cidade);
+		
+		CidadeModel cidadeModel = this.cidadeConverter.toModel(cidade);
+		
+		//cidadeModel.add(new Link("http://localhost:8080/cidades/1", IanaLinkRelations.SELF));
+		cidadeModel.add(new Link("http://localhost:8080/cidades/1"));
+		//cidadeModel.add(new Link("http://localhost:8080/cidades", 
+				 //IanaLinkRelations.COLLECTION));
+		
+		cidadeModel.add(new Link("http://localhost:8080/cidades", "cidades"));
+		
+		cidadeModel.getEstado().add(new Link("http://localhost:8080/estados/1"));
+		return cidadeModel;
 	}
 
 	@PutMapping("/{cidadeId}")
