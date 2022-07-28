227da377a5dca66ede716849c4404cc9bf80a173
BenjaminFernandes <benjamin_fernandes10@hotmail.com>
2022-07-28 19:55:05 -0300

    Adicionando oauth2 password

---
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.oauth2ResourceServer().opaqueToken();
+				
+			/*.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS) //sem session ID, API Stateless.
+			
+				.and()
+					.csrf().disable(); */
+		//csrf evita que o seja usado o sessionId em requisições quando a API não é stateless, se o sessionID é capturado por um hacker ele pode usá-lo para enviar requisições sem as credenciais
+	}
+	
+}
--- src/main/java/com/algaworks/algafood/core/web/ApiRetirementHandler.java
+++ src/main/java/com/algaworks/algafood/core/web/ApiRetirementHandler.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.core.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -13,13 +12,14 @@ public class ApiRetirementHandler extends HandlerInterceptorAdapter{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		if(request.getRequestURI().startsWith("/v1/")) {
+		//o if abaixo é para retornar quando a V1 foi totalmente desabilitada do projeto
+		/*if(request.getRequestURI().startsWith("/v1/")) {
 			//usado para enviar a informação no cabeçalho
 			//response.addHeader("X-Algafood-Deprecated", "Versão deprecidada!");
 			
 			response.setStatus(HttpStatus.GONE.value());
 			return false;
-		}
+		}*/
 		
 		return true;
 	}
