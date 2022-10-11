81ed0332191020d39ec7f34e2a053e9db439ff91
BenjaminFernandes <benjamin_fernandes10@hotmail.com>
2022-10-10 21:51:21 -0300

    Adicionando o authorization server no resource server

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +24,11 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.formLogin()
+			.and()
+			.authorizeRequests()
+				.antMatchers("/oauth/**").authenticated()//utilizado no fluxo authorization code
+			.and()
 			.csrf().disable()
 			.cors()//Spring security permite o OPTIONS 
 			.and()
@@ -62,6 +69,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		
 		
 		return jwtAuthenticationConverter;
+	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 	
 	//Configuração quando utiliza chave simétrica - utiliza o mesmo segredo
--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,14 +15,17 @@ public class WebConfig implements WebMvcConfigurer {
 	@Autowired
 	private ApiRetirementHandler apiRetirementHandler;
 	
-	@Override
+	/*
+	 * Removido na aula 23.41 - implementado na classe CorsConfig
+	 * @Override
+	 
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			//.allowedOrigins("http://localhost:8000");
 			.allowedMethods("*");
 			//.maxAge(30) //em segundos
 	}
-	
+	*/
 	//Aula 17.5 ESR
 	@Bean
 	public Filter shallowEtagHeaderFilter() {
