d820ca11636a0c0fe1e7d8bf74c3f2428608b789
BenjaminFernandes <benjamin_fernandes10@hotmail.com>
2023-01-05 15:57:33 -0300

    Ajustes na criação dos beans de S3 e JavaMailSender

--- src/main/java/com/algaworks/algafood/infraestructure/service/email/FakeEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infraestructure/service/email/FakeEnvioEmailService.java
@@ -1,16 +1,23 @@
 package com.algaworks.algafood.infraestructure.service.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.algaworks.algafood.domain.service.EnvioEmailService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FakeEnvioEmailService extends SmtpEnvioEmailService{
+public class FakeEnvioEmailService implements EnvioEmailService{
 
+	@Autowired
+	private ProcessadorEmailTemplate processadorEmailTemplate;
+	
 	@Override
 	public void enviar(Mensagem mensagem) {
 		
 		log.info("Enviando e-mail fake");
 		
-		String corpo = processarTemplate(mensagem);
+		String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 		
 		log.info("[FAKE E-MAIL] Para: {}\n{}", mensagem.getDestinatarios(), corpo);
 		
--- src/main/java/com/algaworks/algafood/infraestructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infraestructure/service/email/SmtpEnvioEmailService.java
@@ -6,13 +6,9 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 
 public class SmtpEnvioEmailService implements EnvioEmailService {
 
@@ -21,7 +17,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	@Autowired
 	private EmailProperties emailProperties;
 	@Autowired
-	private Configuration freemarkerConfig;
+	private ProcessadorEmailTemplate processadorEmailTemplate;
 	
 	@Override
 	public void enviar(Mensagem mensagem) {
@@ -35,7 +31,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	}
 	
 	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-		String corpo = processarTemplate(mensagem);
+		String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 		
 		MimeMessage mimeMessage = mailSender.createMimeMessage();	
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
@@ -48,15 +44,4 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 		return mimeMessage;
 	}
 
-	protected String processarTemplate(Mensagem mensagem) {
-		try {
-			Template template = this.freemarkerConfig.getTemplate(mensagem.getCorpo());
-			
-			return FreeMarkerTemplateUtils.processTemplateIntoString(template, 
-					mensagem.getVariaveis());
-		} catch (Exception e) {
-			throw new EmailException("Não foi possível montar o template do e-mail", e);
-		} 
-
-	}
 }
