0a74e482ee697ab2e16730893b3e06d8f63f2207
BenjaminFernandes <benjamin_fernandes10@hotmail.com>
2023-03-15 20:00:35 -0300

    removing classes deprecated, add validation and add flyway-mysql

--- src/main/java/com/algaworks/algafood/core/squiggly/TomcatCustomizer.java
+++ src/main/java/com/algaworks/algafood/core/squiggly/TomcatCustomizer.java
@@ -13,7 +13,7 @@ public class TomcatCustomizer implements WebServerFactoryCustomizer<TomcatServle
 
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
-        factory.addConnectorCustomizers(connector -> connector.setAttribute("relaxedQueryChars", "[]"));
+        factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "[]"));
     }
     
 }
--- src/main/java/com/algaworks/algafood/core/web/ApiRetirementHandler.java
+++ src/main/java/com/algaworks/algafood/core/web/ApiRetirementHandler.java
@@ -4,10 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
-public class ApiRetirementHandler extends HandlerInterceptorAdapter{
+public class ApiRetirementHandler implements HandlerInterceptor{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
